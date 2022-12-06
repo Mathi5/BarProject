122bee91aad4eb0a7cb17001f282f48e373166a1
hugrozer <bourgey18@gmail.com>
2022-12-06 17:44:52 +0100

    premier commit, tout pété

---
+++ app/src/main/java/com/example/projetbar/Entities.kt
@@ -0,0 +1,15 @@
+package com.example.projetbar
+
+class Bar {
+    var nom: String = ""
+    var loca: String = ""
+    //var x: Int = 0
+    //var y: Int = 0
+
+    constructor(nom: String, loca: String) {
+        this.nom = nom
+        //this.x = x
+        //this.y = y
+        this.loca = loca
+    }
+}
--- app/src/main/java/com/example/projetbar/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/projetbar/ui/home/HomeViewModel.kt
@@ -1,13 +1,66 @@
 package com.example.projetbar.ui.home
 
+import android.net.http.HttpResponseCache.install
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import com.example.projetbar.Bar
+import com.example.projetbar.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.launch
+
 
 class HomeViewModel : ViewModel() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private var listBars = mutableListOf<Bar>()
+
     var num = 0
     private val _text = MutableLiveData<String>().apply {
         value = num.toString()
     }
     val text: LiveData<String> = _text
+
+    suspend fun getData(url:String){
+
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation){
+                json()
+            }
+        }
+        val responseString = client.get(url).bodyAsText()
+        val jsonObj = JSONObject(responseString)
+        val map = jsonObj.toMap()
+        Log.wtf("wtf", "Json map " + map["2025-01-01"])
+
+
+        for (entry in map.entries.iterator()) {
+            val BarTmp = Bar(entry.key, entry.value.toString())
+            listBars.add(BarTmp)
+        }
+    }
+
+    fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
+        when (val value = this[it])
+        {
+            is JSONArray ->
+            {
+                val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
+                JSONObject(map).toMap().values.toList()
+            }
+            is JSONObject -> value.toMap()
+            JSONObject.NULL -> null
+            else            -> value
+        }
+    }
 }
