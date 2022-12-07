627b9853ff5667770ef2b7609acb5794cde28564
hugrozer <bourgey18@gmail.com>
2022-12-07 14:16:36 +0100

    deuxieme commit, toujours tout pété

--- app/src/main/java/com/example/projetbar/Entities.kt
+++ app/src/main/java/com/example/projetbar/Entities.kt
@@ -1,15 +1,24 @@
 package com.example.projetbar
 
 class Bar {
-    var nom: String = ""
-    var loca: String = ""
+    var name: String = ""
+    var price_level: String = ""
+    var rating: String = ""
+    //var loca: String = ""
     //var x: Int = 0
     //var y: Int = 0
 
-    constructor(nom: String, loca: String) {
-        this.nom = nom
+    constructor(dict:Map<String,*>) {
+        this.name = dict["name"].toString()
+        this.price_level = dict["price_level"].toString()
+        this.rating = dict["rating"].toString()
+
         //this.x = x
         //this.y = y
-        this.loca = loca
+        //this.loca = loca
+        }
+
+    override fun toString(): String {
+        return this.name + " " + this.price_level + " " + this.rating
     }
 }
--- app/src/main/java/com/example/projetbar/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/projetbar/ui/home/HomeViewModel.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.lifecycleScope
 import com.example.projetbar.Bar
 import com.example.projetbar.databinding.ActivityMainBinding
 import org.json.JSONArray
@@ -23,7 +22,7 @@ import kotlinx.coroutines.launch
 class HomeViewModel : ViewModel() {
 
     private lateinit var _binding: ActivityMainBinding
-    private var listBars = mutableListOf<Bar>()
+    var listBars = mutableListOf<Bar>()
 
     var num = 0
     private val _text = MutableLiveData<String>().apply {
@@ -41,13 +40,21 @@ class HomeViewModel : ViewModel() {
         val responseString = client.get(url).bodyAsText()
         val jsonObj = JSONObject(responseString)
         val map = jsonObj.toMap()
-        Log.wtf("wtf", "Json map " + map["2025-01-01"])
+        Log.wtf("wtf", map.toString())
 
+
+        val barTmp = Bar(map)
+        listBars.add(barTmp)
 
         for (entry in map.entries.iterator()) {
-            val BarTmp = Bar(entry.key, entry.value.toString())
-            listBars.add(BarTmp)
+            if (entry.key == "results") {
+                Log.wtf("wtf", entry.key)
+                val testRes = JSONObject(entry.value.toString())
+                val jsonTestRes = testRes.toMap()
+                Log.wtf("wtf", jsonTestRes.toString())
+            }
         }
+
     }
 
     fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
